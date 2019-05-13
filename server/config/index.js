501eba78f29fa5544e59f6b0f2fc7607fde71f9e
Drew Minns <drew@playgroundinc.com>
2019-05-13 17:09:40 -0400

    Added Cypress setup

--- server/config/index.js
+++ server/config/index.js
@@ -1,18 +1,17 @@
+/* eslint-disable no-console */
 /* eslint import/no-unresolved: 0 */
 /* eslint global-require: 0 */
-const chalk = require('chalk');
 
 const PROD = process.env.NODE_ENV === 'production';
 const TEST = process.env.NODE_ENV === 'test';
-const { log } = console;
 
 if (PROD) {
-  log(chalk.whiteBright.bgGreen('Running PRODUCTION'));
+  console.log('**** Running PRODUCTION ****');
   module.exports = require('./prod.js');
 } else if (TEST) {
-  log(chalk.whiteBright.bgYellow('Running TEST'));
+  console.log('**** Running TEST ****');
   module.exports = require('./test.js');
 } else {
-  log(chalk.whiteBright.bgBlueBright('Running DEVELOPMENT'));
+  console.log('**** Running DEVELOPMENT ****');
   module.exports = require('./dev.js');
 }
